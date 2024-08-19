ee9032f24774cdc40cc3e8a8bd99ac28bf2447cb
Ana Lívia Motta <a.motta@aluno.ifsp.edu.br>
2024-08-19 07:43:02 -0300

    feat: Implemented the front-end layout of the project based on the layout guidelines from the activity PDF

--- app/src/main/java/com/example/cadastro/MainActivity.kt
+++ app/src/main/java/com/example/cadastro/MainActivity.kt
@@ -1,10 +1,22 @@
 package com.example.cadastro
 
 import android.os.Bundle
+import android.widget.ArrayAdapter
+import android.widget.Spinner
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
+
+        val spinnerUf: Spinner = findViewById(R.id.ufS)
+
+        val ufs = resources.getStringArray(R.array.ufsList)
+
+        val adapter = ArrayAdapter(this, android.R.layout.simple_spinner_item, ufs)
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item)
+
+        spinnerUf.adapter = adapter
+
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
     }
